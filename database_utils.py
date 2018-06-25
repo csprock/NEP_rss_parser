4a1101ec3770c81fcfe2a307e11e392a415d31e9
csprock <csprock@gmail.com>
2018-06-24 22:43:03 -0500

    changed db connector script in database_utils.py

--- database_utils.py
+++ database_utils.py
@@ -22,14 +22,13 @@ def connect_to_database(conn_info, success_message = True):
 
 def connect_to_heroku_database(conn_url, success_message = True):
     #conn_string = "host='%s' dbname='%s' user='%s' password='%s'" % (conn_info['host'], conn_info['dbname'], conn_info['username'], conn_info['password'])
-    try:
-        conn = psycopg2.connect(conn_url, sslmode = 'require')
-        if success_message is True:
-            print("Connected to database %s." % (conn_info['dbname']))
 
-        return conn
-    except:
-        print('Error! Failure to connect to database %s' % (conn_info['dbname']))
+    conn = psycopg2.connect(conn_url, sslmode = 'require')
+    if success_message is True:
+        print("Connected to database %s." % (conn_info['dbname']))
+
+    return conn
+
 
 def execute_query(conn_obj, query, data = None, return_values = False):
 
