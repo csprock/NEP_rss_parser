bdeb8865d4b0d9333c5cbe947b00f2ed94909b53
csprock <csprock@gmail.com>
2018-06-30 10:32:00 -0500

    removed database files

--- database/version1/initialize_db.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO sources VALUES ('Portland Press Herald', 'Portland, ME');
-
-INSERT INTO feeds VALUES (DEFAULT, 'Portland Press Herald', 'Press Herald News', 'http://www.pressherald.com/category/news/feed/');
-INSERT INTO feeds VALUES (DEFAULT, 'Portland Press Herald', 'Press Herald Business', 'http://www.pressherald.com/category/business/feed/');
--- database/version1/reset_db.sql
+++
@@ -1,6 +0,0 @@
--- reset the content part of the rss database
-
-DELETE FROM place_tags WHERE article_id > 0;
-DELETE FROM article WHERE id > 0;
-ALTER SEQUENCE article_id_seq WITH RESTART;
- 
